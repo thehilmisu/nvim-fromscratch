e07d8eba87a127fd551785a5688929d7ef3d63d6
thehilmisu <ozcanhakanzi@gmail.com>
2025-01-18 23:10:02 +0100

    override the colors for gruber-darker theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,9 +1,21 @@
 return {
-    "blazkowolf/gruber-darker.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = { italic = false,},
-    config = function()
-    vim.cmd.colorscheme("gruber-darker")
-    end
+	"blazkowolf/gruber-darker.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		bold = false,
+		italic = {
+			strings = false,
+			comments = false,
+			operators = false,
+			folds = false,
+		},
+	},
+	config = function()
+		vim.cmd.colorscheme("gruber-darker")
+
+		vim.api.nvim_set_hl(0, "String", { fg = "#73c936", italic = false })
+		vim.api.nvim_set_hl(0, "Character", { fg = "#73c936", italic = false })
+		vim.api.nvim_set_hl(0, "Comment", { fg = "#cc8c3c", italic = false })
+	end,
 }
